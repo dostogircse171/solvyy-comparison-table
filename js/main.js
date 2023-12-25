37b457fe9a2dc6351004dc583ec2f67dd2a663e0
dostogircse171 <dostogircse171@gmail.com>
2023-12-25 00:00:26 +0000

    Fix code alignment

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 
-$( "ul" ).on( "click", "li", function() {
-  var pos = $(this).index()+2;
+$("ul").on("click", "li", function () {
+  var pos = $(this).index() + 2;
   $("tr").find('td:not(:eq(0))').hide();
-  $('td:nth-child('+pos+')').css('display','table-cell');
+  $('td:nth-child(' + pos + ')').css('display', 'table-cell');
   $("tr").find('th:not(:eq(0))').hide();
   $('li').removeClass('active');
   $(this).addClass('active');
 });
-
 // Initialize the media query
-  var mediaQuery = window.matchMedia('(min-width: 640px)');
-  
-  // Add a listen event
-  mediaQuery.addListener(doSomething);
-  
-  // Function to do something with the media query
-  function doSomething(mediaQuery) {    
-    if (mediaQuery.matches) {
-      $('.sep').attr('colspan',5);
-    } else {
-      $('.sep').attr('colspan',2);
-    }
+var mediaQuery = window.matchMedia('(min-width: 640px)');
+// Add a listen event
+mediaQuery.addListener(doSomething);
+
+// Function to do something with the media query
+function doSomething(mediaQuery) {
+  if (mediaQuery.matches) {
+    $('.sep').attr('colspan', 5);
+  } else {
+    $('.sep').attr('colspan', 2);
   }
-  
-  // On load
-  doSomething(mediaQuery);
+}
+// On load
+doSomething(mediaQuery);
